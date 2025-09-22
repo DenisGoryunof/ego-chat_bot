366e19fc3eb7b1e4a9368a9991d7569d38c55e61
DenisGoryunof <sawerD@mail.ru>
2025-09-22 08:15:18 +0300

    1
    1

---
+++ src/features/admin/index.ts
@@ -0,0 +1,4 @@
+
+export { default as AdminPanel } from './components/AdminPanel';
+export { default as AdminLogin } from './components/AdminLogin';
+export { useAdminAuth } from './hooks/useAdminAuth';
